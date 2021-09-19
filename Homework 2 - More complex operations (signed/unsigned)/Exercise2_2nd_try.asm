ab27ad895f3ef822a81f8bcfeb717cb2efb294c8
917_Bogdan_Vlad <72063186+BVlad917@users.noreply.github.com>
2021-09-19 16:58:01 +0300

    Added Homework 2

---
+++ Homework 2 - More complex operations (signed/unsigned)/Exercise2_2nd_try.asm
@@ -0,0 +1,66 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+  
+; 25) (a + b - c) + (a + b + d) - (a + b)  
+; a - byte, b - word, c - double word, d - qword - Signed Representation
+
+
+segment data use32 class=data
+    a db 100
+    b dw 150
+    c dd 350
+    d dq 50
+
+;       ===> -50 for the above data
+
+segment code use32 class=code
+    start:
+        
+        mov al, [a] 
+        cbw; AX = a
+        add ax, [b]
+        cwde; EAX = a + b
+        sub eax, [c]; EAX = a + b - c
+        
+        mov ecx, eax; ECX = a + b - c
+        mov al, [a]
+        cbw
+        add ax, [b]
+        cwde; EAX = a + b
+        cdq; EDX:EAX = a + b
+        
+        clc
+        add dword [d + 0], eax
+        adc dword [d + 4], edx; qword @ address d = (a + b + d)
+        
+        mov eax, ecx
+        cdq; EDX:EAX = a + b - c
+        
+        clc
+        add dword [d + 0], eax
+        adc dword [d + 4], edx; qword @ address d = (a + b - c) + (a + b + d)
+        
+        mov al, [a]
+        cbw
+        add ax, [b]
+        cwde
+        cdq; EDX:EAX = a + b
+        
+        clc
+        sub dword [d + 0], eax
+        sbb dword [d + 4], edx; qword @ address d = (a + b - c) + (a + b + d) - (a + b)
+        
+        mov eax, dword [d + 0]
+        mov edx, dword [d + 4]; EDX:EAX = (a + b - c) + (a + b + d) - (a + b)
+        
+        
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
---
+++ Homework 2 - More complex operations (signed/unsigned)/Exercise4.asm
@@ -0,0 +1,70 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; (a * a + b + x) / (b + b) + c * c; - Signed interpretation
+; a - word; b - byte; c - doubleword; x - qword
+                          
+segment data use32 class=data
+    a dw -5
+    b db 2
+    c dd 3
+    x dq -89
+    
+; In the above data, the result is: (25 + 2 + (-67)) / (2 + 2) + 3 * 3 = -40 / 4 + 9 = -10 + 9 = -1
+
+segment code use32 class=code
+    start:
+        xor eax, eax
+        mov ax, [a]
+        
+        imul ax; DX:AX = a * a
+        push dx
+        push ax
+        pop ebx; EBX = a * a
+        
+        mov al, [b]
+        cbw
+        cwde; EAX = b
+        
+        add eax, ebx; EAX = a * a + b
+        cdq; EDX:EAX = a * a + b
+        
+        add dword [x], eax
+        adc dword [x + 4], edx
+        ; Now the address of qword x has (a * a + b + x)
+        
+        mov al, [b]
+        cbw
+        cwde; EAX = b
+        add eax, eax; EAX = b + b
+        
+        mov ebx, eax; EBX = b + b
+        
+        mov edx, dword [x + 4]
+        mov eax, dword [x]; EDX:EAX = (a * a + b + x)
+        
+        idiv ebx; EAX = (a * a + b + x) / (b + b); EDX = (a * a + b + x) % (b + b)
+        mov ebx, eax; EBX = (a * a + b + x) / (b + b)
+        
+        mov eax, [c]
+        imul eax; EDX:EAX = c * c
+        
+        mov ecx, eax
+        mov eax, ebx
+        mov ebx, edx
+        ; Now EBX:ECX = (c * c)
+        cdq; EDX:EAX = (a * a + b + x) / (b + b)
+        
+        add eax, ecx
+        adc edx, ebx
+        ; Now EDX:EAX has the answer: -1
+        
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
